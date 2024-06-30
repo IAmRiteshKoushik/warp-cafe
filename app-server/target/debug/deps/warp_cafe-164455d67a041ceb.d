65d760d72d3d715a2d14e7af6019d81dffb8be0c
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-06-30 14:13:41 +0530

    Update gitignore

--- app-server/target/debug/deps/warp_cafe-164455d67a041ceb.d
+++
@@ -1,9 +0,0 @@
-/home/rk/workspace/github.com/IAmRiteshKoushik/focus/warp-cafe/app-server/target/debug/deps/libwarp_cafe-164455d67a041ceb.rmeta: src/main.rs src/db.rs src/handlers.rs src/models.rs src/routes.rs
-
-/home/rk/workspace/github.com/IAmRiteshKoushik/focus/warp-cafe/app-server/target/debug/deps/warp_cafe-164455d67a041ceb.d: src/main.rs src/db.rs src/handlers.rs src/models.rs src/routes.rs
-
-src/main.rs:
-src/db.rs:
-src/handlers.rs:
-src/models.rs:
-src/routes.rs:
--- app-server/target/debug/deps/warp_cafe-9d0434ce4247d54d.d
+++
@@ -1,9 +0,0 @@
-/home/rk/workspace/github.com/IAmRiteshKoushik/focus/warp-cafe/app-server/target/debug/deps/libwarp_cafe-9d0434ce4247d54d.rmeta: src/main.rs src/db.rs src/handlers.rs src/models.rs src/routes.rs
-
-/home/rk/workspace/github.com/IAmRiteshKoushik/focus/warp-cafe/app-server/target/debug/deps/warp_cafe-9d0434ce4247d54d.d: src/main.rs src/db.rs src/handlers.rs src/models.rs src/routes.rs
-
-src/main.rs:
-src/db.rs:
-src/handlers.rs:
-src/models.rs:
-src/routes.rs:
